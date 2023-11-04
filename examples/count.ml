c46395afbc60cc55c38cf35b4e5e70925ee029f7
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-11-04 16:47:39 -0400

    rename to "fuseau"

--- examples/count.ml
+++ examples/count.ml
@@ -1,5 +1,3 @@
-module L2L = Luv2loop
-
 let spf = Printf.sprintf
 
 let main () =
@@ -7,15 +5,15 @@ let main () =
 
   let comps =
     Array.init 4 (fun j ->
-        L2L.spawn @@ fun () ->
+        Fuseau.spawn @@ fun () ->
         print_endline @@ spf "spawn fiber %d" j;
         for i = 1 to 10 do
-          L2L.Timer.sleep_ms 100;
+          Fuseau.Timer.sleep_ms 100;
           print_endline @@ spf "iter %d (fiber %d)" i j
         done)
   in
 
-  Array.iter L2L.Computation.await comps;
+  Array.iter Fuseau.Computation.await comps;
   print_endline "done"
 
-let () = L2L.main main
+let () = Fuseau.main main
