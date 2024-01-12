32843451f9b87ba8949cf9a1115eb3b0abd9f134
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-01-11 21:26:21 -0500

    lib: add some utils

--- src/lib/sleep.ml
+++ src/lib/sleep.ml
@@ -7,7 +7,7 @@ let sleep delay : unit =
     let loop = Scheduler.Internal_.ev_loop sched in
     Fiber.Internal_.suspend ~before_suspend:(fun ~wakeup ->
         ignore
-          (loop#on_timer ~repeat:false delay (fun _ev -> wakeup ())
+          (Event_loop.on_timer loop ~repeat:false delay (fun _ev -> wakeup ())
             : event_handle);
         ())
   )
