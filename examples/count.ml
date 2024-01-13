a6087669c94a65e720ecbc830ff78fc3a9d629e1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-01-12 23:19:33 -0500

    update example

--- examples/count.ml
+++ examples/count.ml
@@ -8,12 +8,14 @@ let main () =
         Fuseau.spawn @@ fun () ->
         print_endline @@ spf "spawn fiber %d" j;
         for i = 1 to 10 do
-          Fuseau.Timer.sleep_ms 100;
+          Fuseau.sleep 0.1;
           print_endline @@ spf "iter %d (fiber %d)" i j
         done)
   in
 
-  Array.iter Fuseau.Computation.await comps;
+  Array.iter Fuseau.await comps;
   print_endline "done"
 
-let () = Fuseau.main main
+let () =
+  let loop = new Fuseau_unix.ev_loop in
+  Fuseau.main ~loop main
