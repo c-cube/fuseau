621649c6ab34c8663f29bba9fc2430f3fc723a5d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-01-28 00:10:25 -0500

    wip: more fuseau-lwt

--- src/lwt/fuseau_lwt.ml
+++ src/lwt/fuseau_lwt.ml
@@ -33,6 +33,17 @@ class ev_loop (engine : Lwt_engine.t) : Event_loop.t =
     (* Printf.printf "lwt one step done %a\n%!" _pp_pending engine *)
   end
 
+(* TODO:
+   - [spawn_as_lwt : (unit -> 'a) -> 'a Lwt.t]
+     (creates a lwt/lwt-promise, spawns a fiber that eventually fulfill promise,
+       return lwt)
+
+   - [spawn_as_lwt_as_child_of : _ fiber -> (unit -> 'a) -> 'a Lwt.t]
+       same but with explicit ownership
+
+   - [wait_fiber : 'a Fiber.t -> 'a Lwt.t]
+*)
+
 (** The global loop using {!Lwt_engine.get()} *)
 let create () : Event_loop.t =
   let engine : Lwt_engine.t = Lwt_engine.get () in
@@ -53,3 +64,25 @@ let await (fut : _ Lwt.t) =
     (match Lwt.poll fut with
     | Some x -> x
     | None -> assert false)
+
+let spawn_as_lwt ?name ?propagate_cancel_to_parent (f : unit -> 'a) : 'a Lwt.t =
+  let fut, promise = Lwt.wait () in
+  let _fib =
+    Fuseau.spawn ?name ?propagate_cancel_to_parent (fun () ->
+        try
+          let x = f () in
+          Lwt.wakeup promise x
+        with exn -> Lwt.wakeup_exn promise exn)
+  in
+  fut
+
+let spawn_as_lwt_from_anywhere ?name sched f : 'a Lwt.t =
+  let fut, promise = Lwt.wait () in
+  let _fib =
+    Fuseau.spawn_from_anywhere ?name sched (fun () ->
+        try
+          let x = f () in
+          Lwt.wakeup promise x
+        with exn -> Lwt.wakeup_exn promise exn)
+  in
+  fut
--- src/lwt/fuseau_lwt.mli
+++ src/lwt/fuseau_lwt.mli
@@ -1,5 +1,11 @@
 val await : 'a Lwt.t -> 'a
 (** Like {!Fuseau.await} but on a Lwt promise. *)
 
+val spawn_as_lwt :
+  ?name:string -> ?propagate_cancel_to_parent:bool -> (unit -> 'a) -> 'a Lwt.t
+
+val spawn_as_lwt_from_anywhere :
+  ?name:string -> Scheduler.t -> (unit -> 'a) -> 'a Lwt.t
+
 val main : (unit -> 'a) -> 'a
 (** Run main loop, using the current [Lwt_engine.t]. *)
