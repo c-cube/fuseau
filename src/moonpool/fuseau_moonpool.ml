0bf2f906302347bdd857d9c7453398d066a61f9d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-17 12:45:42 -0500

    feat moonpool: add a `spawn` helper

--- src/moonpool/fuseau_moonpool.ml
+++ src/moonpool/fuseau_moonpool.ml
@@ -9,3 +9,6 @@ let await_fut (fut : _ Fut.t) =
     Fuseau.Private_.suspend ~before_suspend:(fun ~wakeup ->
         Fut.on_result fut (fun _ -> wakeup ()));
     Fut.get_or_fail_exn fut
+
+let[@inline] spawn ~on f = Fut.spawn ~on f
+let[@inline] spawn_and_await ~on f = await_fut @@ spawn ~on f
--- src/moonpool/fuseau_moonpool.mli
+++ src/moonpool/fuseau_moonpool.mli
@@ -1,4 +1,12 @@
 (** Interoperability between Fuseau and Moonpool *)
 
 val await_fut : 'a Moonpool.Fut.t -> 'a
-(** [await_fut fut] suspends the current fiber until the given future is done *)
+(** [await_fut fut] suspends the current fiber until
+    the given future is done. The future [fut] is a thread-safe
+    Moonpool future (e.g. obtained from {!Moonpool.Fut.spawn})
+    but this must be called from inside a fuseau thread. *)
+
+val spawn : on:Moonpool.Runner.t -> (unit -> 'a) -> 'a Moonpool.Fut.t
+(** An alias to {!Moonpool.Fut.spawn} *)
+
+val spawn_and_await : on:Moonpool.Runner.t -> (unit -> 'a) -> 'a
