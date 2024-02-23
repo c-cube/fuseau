59ab317e18d7cf3672e246d57e75969039e77a10
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-22 23:42:04 -0500

    more docs

--- src/lwt/fuseau_lwt.mli
+++ src/lwt/fuseau_lwt.mli
@@ -12,16 +12,28 @@ include module type of struct
   include Fuseau
 end
 
-(** {2 Interop *)
+(** {2 Interop with Lwt}
+
+    Here we have functions that are used to cross
+    the boundary between the Fuseau world
+    and the Lwt world. *)
 
 val await_lwt : 'a Lwt.t -> 'a
 (** Like {!Fuseau.await} but on a Lwt promise. *)
 
 val spawn_as_lwt :
-  ?name:string -> ?propagate_cancel_to_parent:bool -> (unit -> 'a) -> 'a Lwt.t
+  ?parent:_ Fiber.t ->
+  ?name:string ->
+  ?propagate_cancel_to_parent:bool ->
+  (unit -> 'a) ->
+  'a Lwt.t
+(** [spawn_as_lwt f] runs [f()] in the Fuseau+Lwt thread, and returns
+    a Lwt future that resolves when the fiber does *)
 
 val spawn_as_lwt_from_anywhere :
   ?name:string -> Scheduler.t -> (unit -> 'a) -> 'a Lwt.t
+(** [spawn_from_anywhere scheduler f] runs [f()] as a toplevel fiber
+      in [scheduler]. It can be run from another thread. *)
 
 (** IO through Lwt's engine *)
 module IO_lwt : sig
@@ -33,8 +45,17 @@ module IO_lwt : sig
 end
 
 val ev_read : Unix.file_descr -> bytes -> int -> int -> int Event.t
-val ev_write : Unix.file_descr -> bytes -> int -> int -> int Event.t
+(** [ev_read fd buf i len] is an event that, when ready,
+    will ready at most [len] bytes from [fd] into [buf[i.. i+len]],
+    and return how many bytes were read. It uses [Lwt_engine]
+    to wait for [fd]'s readiness. *)
 
+val ev_write : Unix.file_descr -> bytes -> int -> int -> int Event.t
+(** [ev_write fd buf i len] is an event that, when ready,
+    writes at most [len] bytes from [buf] into [fd]. It uses
+    [Lwt_engine] to wait for [fd]'s readiness. *)
+
+(** Iostream specialized for Lwt *)
 module Iostream : sig
   module In : sig
     include module type of Iostream.In
@@ -49,9 +70,11 @@ module Iostream : sig
   end
 end
 
+(** Networking using [Lwt_io] *)
 module Net : sig
   module TCP_server : sig
     type t = Lwt_io.server
+    (** The Lwt TCP server *)
 
     val establish :
       ?backlog:int ->
@@ -59,6 +82,8 @@ module Net : sig
       Unix.sockaddr ->
       (Unix.sockaddr -> Iostream.In.t -> Iostream.Out.t -> unit) ->
       t
+    (** Establish a server, where each connection spawns a new
+        fiber that uses Fuseau's Iostreams. *)
 
     val shutdown : t -> unit
   end
@@ -66,6 +91,7 @@ module Net : sig
   module TCP_client : sig
     val with_connect :
       Unix.sockaddr -> (Iostream.In.t -> Iostream.Out.t -> 'a) -> 'a
+    (** Use Lwt to connect over TCP *)
   end
 end
 
